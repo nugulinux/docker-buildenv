11ede02a958fb1e7ec690b223d2d578113a28504
Inho Oh <inho.oh@sk.com>
2019-07-15 13:35:40 +0900

    add -d option to sbuild.sh
    Add the 'd' option to use DIRECT for proxy config.
    
    Signed-off-by: Inho Oh <inho.oh@sk.com>

--- sbuild.sh
+++ sbuild.sh
@@ -3,16 +3,19 @@
 . /etc/buildinfo
 JOBS=`nproc`
 
-while getopts ":p:" o; do
-    case "${o}" in
-        p)
-	    APTPROXY="Acquire::http::Proxy \"${OPTARG}\";"
-            ;;
-        *)
-            echo "Usage: $0 [-p apt_cache_proxy_url] [sbuild options]"
-	    exit 1
-            ;;
-    esac
+while getopts ":p:d:" o; do
+	case "${o}" in
+		p)
+			APTPROXY="Acquire::http::Proxy \"${OPTARG}\";"
+			;;
+		d)
+			DIRECT="Acquire::http::Proxy::${OPTARG} \"DIRECT\";"
+			;;
+		*)
+			echo "Usage: $0 [-p apt_cache_proxy_url] [-d direct_url] [sbuild options]"
+			exit 1
+			;;
+	esac
 done
 shift $((OPTIND-1))
 
@@ -21,5 +24,9 @@ if [ ! -z "$APTPROXY" ]; then
 	echo $RESULT
 fi
 
+if [ ! -z "$DIRECT" ]; then
+	RESULT=$(echo $DIRECT | sudo tee -a /var/lib/schroot/chroots/$CHROOT/etc/apt/apt.conf.d/proxy)
+	echo $RESULT
+fi
 
 sbuild --chroot $CHROOT --host $HOST -j$JOBS --dpkg-source-opt="-I.git" "$@"
